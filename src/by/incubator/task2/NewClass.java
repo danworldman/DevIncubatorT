c4dc84040b2ce73b73ceaac3adb3451794a44a1c
Daniil Pachko <danila.pachko@gmail.com>
2022-09-13 09:09:21 +0300

    madeseparatestringvariable

--- src/by/incubator/task2/NewClass.java
+++ src/by/incubator/task2/NewClass.java
@@ -1,24 +1,26 @@
 package by.incubator.task2;
 
 import by.incubator.Main;
+
 import java.net.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class NewClass {
     public static void main(String[] args) throws MalformedURLException, ClassNotFoundException {
-        URL url = new URL("file:///D: // javaprog // fortask2 // out // example.jar");
-        URLClassLoader myClassLoader = new URLClassLoader(new URL[] { url } );
-        Class myClass  =  myClassLoader.loadClass("by.incubator.Main");
+        String filePath = "file:///D: // javaprog // fortask2 // out // example.jar";
+        URL url = new URL(filePath);
+        URLClassLoader myClassLoader = new URLClassLoader(new URL[]{url});
+        Class myClass = myClassLoader.loadClass("by.incubator.Main");
 
-        try{
+        try {
             Main instance = (Main) myClass.newInstance();
 
             instance.main(new String[]{});
-        } catch (InstantiationException e){
+        } catch (InstantiationException e) {
             e.printStackTrace();
 
-        }catch (IllegalAccessException e){
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
     }

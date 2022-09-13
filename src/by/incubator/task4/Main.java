909a71faae97c2a40aa5297e458ad87dbd9e4240
Daniil Pachko <danila.pachko@gmail.com>
2022-09-13 15:20:33 +0300

    made separate print methods

--- src/by/incubator/task4/Main.java
+++ src/by/incubator/task4/Main.java
@@ -5,25 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int numberOfDay = scannerNumber();
-        dayOfWeek(numberOfDay);
+        printScannerNumber(numberOfDay);
+        boolean result = dayOfWeek(numberOfDay);
+        printInfoOfDay(result, numberOfDay);
     }
 
-    public static void dayOfWeek(int numberOfDay) {
-        Weekday[] weekday = Weekday.values();
+    public static boolean dayOfWeek(int numberOfDay) {
         if (numberOfDay >= 1 && numberOfDay <= 7) {
-            System.out.println(weekday[numberOfDay - 1] + " (" + weekday[numberOfDay - 1].getShortName() + ") " +
-                    (weekday[numberOfDay - 1].isWeekend() ? "is a weekend" : "is not a weekend"));
+            return true;
         } else {
-            int notDay = numberOfDay;
-            System.out.print("Entered the wrong day of the week number: ");
-            System.out.println(notDay);
+            return false;
         }
     }
 
-    public static int scannerNumber(){
+    public static int scannerNumber() {
         Scanner scanner = new Scanner(System.in);
         int numberDay = scanner.nextInt();
-        System.out.println("The number of day: " + numberDay);
         return numberDay;
+    }
+
+    public static void printScannerNumber(int numberDay) {
+        System.out.println("The number of day: " + numberDay);
+    }
+
+    public static void printInfoOfDay(boolean res, int numberOfDay) {
+        Weekday[] weekday = Weekday.values();
+        if (res == true) {
+            System.out.println(weekday[numberOfDay - 1] + " (" + weekday[numberOfDay - 1].getShortName() + ") " +
+                    (weekday[numberOfDay - 1].isWeekend() ? "is a weekend" : "is not a weekend"));
+        } else {
+            System.out.print("Entered the wrong day of the week number: ");
+            System.out.println(numberOfDay);
+        }
     }
 }

345e3f9102ebe43252b725e89e02748710ad2898
Daniil Pachko <danila.pachko@gmail.com>
2022-09-13 09:02:47 +0300

    madetwomethods

--- src/by/incubator/task4/Main.java
+++ src/by/incubator/task4/Main.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        dayOfWeek();
+        int numberOfDay = scannerNumber();
+        dayOfWeek(numberOfDay);
     }
 
-    public static void dayOfWeek() {
-        Scanner scanner = new Scanner(System.in);
-        int numberOfDay = scanner.nextInt();
-        System.out.println(numberOfDay);
+    public static void dayOfWeek(int numberOfDay) {
         Weekday[] weekday = Weekday.values();
-
         if (numberOfDay >= 1 && numberOfDay <= 7) {
             System.out.println(weekday[numberOfDay - 1] + " (" + weekday[numberOfDay - 1].getShortName() + ") " +
                     (weekday[numberOfDay - 1].isWeekend() ? "is a weekend" : "is not a weekend"));
@@ -21,5 +18,12 @@ public class Main {
             System.out.print("Entered the wrong day of the week number: ");
             System.out.println(notDay);
         }
+    }
+
+    public static int scannerNumber(){
+        Scanner scanner = new Scanner(System.in);
+        int numberDay = scanner.nextInt();
+        System.out.println("The number of day: " + numberDay);
+        return numberDay;
     }
 }
